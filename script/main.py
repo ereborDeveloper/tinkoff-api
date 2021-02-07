cd3ab0225181fcc4576e045cd7cadb1095d0cd62
Vitas <Izl_2245739>
2021-02-08 00:59:27 +0300

    Первые попыткы. Создана функция Получение портфеля клиента Создана функция Выставление баланса по валютным позициям Создана функция Выставление баланса по инструментным позициям

--- script/main.py
+++ script/main.py
@@ -1,9 +1,34 @@
 import tinvest
+import KEY
+
+TOKEN=KEY.TOKENTIN
+
+print(type(TOKEN))
+def SetCurrency(tokenCl,brokerAccountId):
+    client = tinvest.SyncClient(tokenCl,use_sandbox=True)
+    body = tinvest.SandboxSetCurrencyBalanceRequest(
+        balance=1000,
+        currency='RUB',
+    )
+    client.set_sandbox_currencies_balance(body,brokerAccountId)
 
 
-def getStocks():
+
+
+def SetPosition(tokenCl, brokerAccountId):
+    client = tinvest.SyncClient(tokenCl,use_sandbox=True)
+    body = tinvest.SandboxSetPositionBalanceRequest(
+        balance=1000,
+        figi='BBG0013HGFT4',
+        )
+    client.set_sandbox_positions_balance(body, brokerAccountId)
+
+
+
+
+def getStocks(tokenCl):
     stocks = {}
-    client = tinvest.SyncClient(TOKEN)
+    client = tinvest.SyncClient(tokenCl, use_sandbox=True)
     response = client.get_portfolio()
     positions = response.payload.positions
 
@@ -13,3 +38,5 @@ def getStocks():
         stocks[key] = float(value)
 
     return stocks
+
+getStocks(TOKEN)
