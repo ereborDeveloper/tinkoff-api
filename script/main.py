b290cb28d4ba7d508e93f31ce3689dc75eb2a7c9
Vitas <Izl_2245739>
2021-02-08 02:20:04 +0300

    Первые попыткы. Создана функция Получение портфеля клиента Создана функция Выставление баланса по валютным позициям Создана функция Выставление баланса по инструментным позициям Создана функция Получения ID Брокера Создана функция Получение списка акций + цена закрытия

--- script/main.py
+++ script/main.py
@@ -1,9 +1,16 @@
+import time
+
 import tinvest
+from pprint import pprint
 import KEY
 
 TOKEN=KEY.TOKENTIN
+def getBrokerId(tokenCl):
+    client =tinvest.SyncClient(tokenCl, use_sandbox=True)
+    return client.get_accounts().payload.accounts[0].broker_account_id
 
-print(type(TOKEN))
+BrokerAccountId=getBrokerId(TOKEN)
+
 def SetCurrency(tokenCl,brokerAccountId):
     client = tinvest.SyncClient(tokenCl,use_sandbox=True)
     body = tinvest.SandboxSetCurrencyBalanceRequest(
@@ -15,11 +22,11 @@ def SetCurrency(tokenCl,brokerAccountId):
 
 
 
-def SetPosition(tokenCl, brokerAccountId):
+def SetPosition(tokenCl, brokerAccountId, MoneyBalans, StocksFIGI):
     client = tinvest.SyncClient(tokenCl,use_sandbox=True)
     body = tinvest.SandboxSetPositionBalanceRequest(
-        balance=1000,
-        figi='BBG0013HGFT4',
+        balance=MoneyBalans,
+        figi=StocksFIGI,
         )
     client.set_sandbox_positions_balance(body, brokerAccountId)
 
@@ -30,13 +37,37 @@ def getStocks(tokenCl):
     stocks = {}
     client = tinvest.SyncClient(tokenCl, use_sandbox=True)
     response = client.get_portfolio()
-    positions = response.payload.positions
+    if response.status == 'Ok':
+        positions = response.payload.positions
+        pprint(positions)
+        for i in positions:
+            key = i.name
+            if i.average_position_price==None:
+                continue
+            value = i.average_position_price.value
 
-    for i in positions:
-        key = i.name
-        value = i.average_position_price.value
-        stocks[key] = float(value)
+            stocks[key] = float(value)
 
     return stocks
 
-getStocks(TOKEN)
+def getStocks2(tokenCl):
+    client = tinvest.SyncClient(tokenCl, use_sandbox=True)
+    response = client.get_portfolio()
+    positions = response.payload.positions
+    print(positions)
+
+def getMarketStocks(tokenCl):
+    client =  tinvest.SyncClient(tokenCl, use_sandbox=True)
+    mass=client.get_market_stocks()
+    for i in mass.payload.instruments:
+        client.get_market_orderbook(i.figi, 1)
+        if client.get_market_orderbook(i.figi, 1).status=="Ok":
+            print("Навазние Акции: ",i.name, "  Цена закрытия:",client.get_market_orderbook(i.figi, 5).payload.close_price," ", i.currency.value )
+            time.sleep(0.2)
+
+pprint("Hello")
+
+# SetPosition(TOKEN, BrokerAccountId, 5, "BBG000HLJ7M4")
+
+getMarketStocks(TOKEN)
+
